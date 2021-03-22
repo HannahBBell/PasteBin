1292ffba301e2102631e3c731ca745b35cdc27f2
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-03-22 15:04:47 +0000

    get and connection to database complete

--- server.ts
+++ server.ts
@@ -1,17 +1,29 @@
 import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
+import {Client} from "pg";
 
-// const express = require("express");
-// const cors = require("cors");
-
+dotenv.config();
 const app = express();
 
 app.use(express.json()); //parse json body in requests
 app.use(cors());
 
-app.get("/posts", (req, res) => {
-    res.json({message: "hello", date: "today"});
-})
+const client = new Client({
+    connectionString: process.env.URI,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect()
+
+app.get("/posts", async(req, res) => {
+    const allPosts = await client.query("SELECT * FROM posts");
+    // res.json(allPosts.rows);
+    console.log(allPosts);
+    res.json({message: "hello", data: allPosts.rows });
+});
 
 app.post("/posts", (req, res) => {
     const recieveddata = req.body;
