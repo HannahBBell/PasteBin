6f4410ff469f91dd172972bbe24e16e38c1a5b64
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-03-22 11:51:18 +0000

    post and get requests added

--- server.ts
+++ server.ts
@@ -1,1 +1,32 @@
-console.log("Hello");
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(express.json()); //parse json body in requests
+app.use(cors());
+
+app.get("/posts", (req, res) => {
+    res.json({message: "hello", date: "today"});
+})
+
+app.post("/posts", (req, res) => {
+    const recieveddata = req.body;
+    res.json({message:"Thanks for your response", datarecieved: recieveddata})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, () => {
+    console.log("server has started on port 4000")
+});
