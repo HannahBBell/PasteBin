2bb360833866b2a56b42a3ed302d84f15189d959
Hannah Bernard-Bell <hannah.bernard12@gmail.com>
2021-03-22 12:17:54 +0000

    POST and GET requests set up

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 import express from "express";
 
+// const express = require("express");
+// const cors = require("cors");
+
 const app = express();
 
 app.use(express.json()); //parse json body in requests
@@ -14,18 +17,6 @@ app.post("/posts", (req, res) => {
     const recieveddata = req.body;
     res.json({message:"Thanks for your response", datarecieved: recieveddata})
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(4000, () => {
     console.log("server has started on port 4000")
